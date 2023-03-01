9228034044b46479f25f8d4590745e975c2f35eb
Akhil Varma <akhil@appsmith.com>
2023-03-01 10:52:37 +0000

    System generated commit, after creating a new branch: git-testing

---
+++ pages/Home/jsobjects/openAICompletion/openAICompletion.js
@@ -0,0 +1,11 @@
+export default {
+	openAICompletion: async () => {
+
+		
+		{{adaCompletion.run(() => resetWidget("adaResponse",true), () => {})}}
+		{{babbageCompletion.run(() => resetWidget("babbageResponse",true), () => {})}}
+		{{curieCompletion.run(() => resetWidget("curieResponse",true), () => {})}}
+		{{davinciCompletion.run(() => resetWidget("davinciResponse",true), () => {})}}
+	}
+}
+
