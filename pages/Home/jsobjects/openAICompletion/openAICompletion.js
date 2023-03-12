afda8f07bc1a153b2547cb735a761575fe8a70d2
Akhil Varma <akhil@appsmith.com>
2023-03-12 08:05:28 +0000

    Small fixes

--- pages/Home/jsobjects/openAICompletion/openAICompletion.js
+++ pages/Home/jsobjects/openAICompletion/openAICompletion.js
@@ -1,7 +1,5 @@
 export default {
 	openAICompletion: async () => {
-
-		
 		{{adaCompletion.run(() => resetWidget("adaResponse",true), () => {})}}
 		{{babbageCompletion.run(() => resetWidget("babbageResponse",true), () => {})}}
 		{{curieCompletion.run(() => resetWidget("curieResponse",true), () => {})}}
